f6c00f10d61edf7a91f78d32e7333ad12719f12a
mdutry <matthieu.dutry@hotmail.fr>
2021-03-11 14:34:41 +0100

    App.js mis à jour avec route

--- todofilm/src/App.js
+++ todofilm/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Accueil from './pages/accueil/Accueil'
+import Connexion from './pages/connexion/Connexion'
 import Inscription from './pages/inscription/Inscription'
+import MdPOublie from './pages/MdP oublie/MdPOublie'
+import PageRecherche from './pages/recherche/PageRecherche'
+import PageFilm from './pages/page film/PageFilm'
+import PageProfile from './pages/profilUser/PageProfile'
+import PageWishlist from './pages/wishlist/PageWishlist'
+import PageErreur from './pages/erreur/PageErreur'
 
 function App() {
   return (
-    <div className="App">
-        
-    <Inscription />
-    
-  </div>
+    <Router>
+
+      <Switch>
+        <Route exact path="/" component={Accueil} />
+        <Route path="/connexion" component={Connexion} />
+        <Route path="/inscription" component={Inscription} />
+        <Route path="/mot-de-passe-oublie" component={MdPOublie} />
+        <Route path="/recherche" component={PageRecherche} />
+        <Route path="/film" component={PageFilm} />
+        <Route path="/profil" component={PageProfile} />
+        <Route path="/wishlist" component={PageWishlist} />
+        <Route component={PageErreur} />
+      </Switch>
+      
+    </Router>
   );
 }
 
