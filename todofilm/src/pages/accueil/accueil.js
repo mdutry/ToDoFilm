d113cc0e7421fcb2a0deb09ac3ab6e1316e1a66a
mdutry <matthieu.dutry@hotmail.fr>
2021-03-12 14:44:07 +0100

    commit composant Accueil de Fabrice

--- todofilm/src/pages/accueil/accueil.js
+++ todofilm/src/pages/accueil/accueil.js
@@ -1,1 +1,31 @@
- 
+import React from 'react';
+import { Link} from 'react-router-dom';
+import InputComponent from '../../components/InputComponent'
+import Logo from '../../components/Logo'
+import '../../styles/general.css'
+import './Accueil.css'
+
+function Accueil() {
+    return (
+        
+        <div className='bloc-identify background-yellow'>
+            <div className='full-logo'>
+                <Logo />
+            </div>
+
+            <div className='bloc-identify-input'>
+                <Link to="/connexion" className="bloc-identify-link-input">
+                    <InputComponent type="button" value="Connexion"  />
+                </Link>
+                <Link to ="/inscription" className="bloc-identify-link-input">
+                    <InputComponent type="button" value="Inscription"  />
+                </Link>
+                <Link to="/mot-de-passe-oublie" className="bloc-identify-Mdpo">
+                    Mot de passe oublié
+                </Link>
+            </div>
+        </div>
+    )
+    
+}
+export default Accueil
