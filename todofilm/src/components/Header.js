2449f9c1d83709a5b46253f116ba61d206976b21
mdutry <matthieu.dutry@hotmail.fr>
2021-03-17 11:08:42 +0100

    modif header pour envoi sur main

--- todofilm/src/components/Header.js
+++ todofilm/src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Link} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import InputComponent from './InputComponent';
 import {datasUser} from '../datas/datasUser';
 import logo_ToDoFilm_blanc from '../images/logo_ToDoFilm_blanc.png';
@@ -7,29 +7,26 @@ import avatar from '../images/avatar.png';
 import './Header.css';
 
 
-function Header ()  {
-    return (
-       <div className='bloc-header'>
+function Header () {
+   return (
+      <div className='bloc-header'>
 
          <div className='bloc-header-logo'>
-            < img src={logo_ToDoFilm_blanc} value='ToDoFilm' />
+            <img src={logo_ToDoFilm_blanc} value='ToDoFilm' />
             <p>ToDoFilm</p>
          </div>   
 
-         <Link to='/recherche' className='input-bloc-header-recherche'>
+         <Link to='/recherche' className='bloc-header-recherche'>
             <InputComponent type='button' />
-            
          </Link>   
 
          <Link to='/profil' className='bloc-header-profil'>
-            <img src={avatar} />
-            
-            <p > Prénom </p>
+            <img src={avatar} alt="photo de profil" />
+            <p>prénom : {datasUser.prenom}</p>
          </Link>
 
-       </div>  
-        
-    )
-
+      </div>
+   )
 }
-export default Header; 
+
+export default Header;
