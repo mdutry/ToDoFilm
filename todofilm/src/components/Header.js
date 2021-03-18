2efc61ae84dbafeff439bac30aa6cae14fe2f2ec
mdutry <matthieu.dutry@hotmail.fr>
2021-03-18 14:34:37 +0100

    header maj

--- todofilm/src/components/Header.js
+++ todofilm/src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import InputComponent from './InputComponent';
 import {datasUser} from '../datas/datasUser';
 import logo_ToDoFilm_blanc from '../images/logo_ToDoFilm_blanc.png';
 import avatar from '../images/avatar.png';
+import loupe from '../images/loupe.png';
 import './Header.css';
 
 
@@ -12,21 +12,27 @@ function Header () {
       <div className='bloc-header'>
 
          <div className='bloc-header-logo'>
-            <img src={logo_ToDoFilm_blanc} value='ToDoFilm' />
+            <img src={logo_ToDoFilm_blanc} alt='logo ToDoFilm' />
             <p>ToDoFilm</p>
          </div>   
 
-         <Link to='/recherche' className='bloc-header-recherche'>
-            <InputComponent type='button' />
-         </Link>   
+         <div className='bloc-header-recherche'>
+            <Link to='/recherche'>
+               <img src={loupe} alt="loupe" />
+            </Link>
+         </div>   
 
-         <Link to='/profil' className='bloc-header-profil'>
-            <img src={avatar} alt="photo de profil" />
-            <p>prénom : {datasUser.prenom}</p>
-         </Link>
+         <div className='bloc-header-profil'>
+            <div>
+               <Link to='/profil'>
+                  <img src={avatar} alt="avatar" />
+                  {datasUser.prenom}
+               </Link>
+            </div>
+         </div>
 
       </div>
    )
 }
 
-export default Header;
+export default Header; 
