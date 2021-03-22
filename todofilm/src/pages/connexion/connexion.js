f67326d6f079a2d20d97c4b673d29a30ae21782e
Fvandecasteele <fabrice.vandecasteele@sfr.fr>
2021-03-22 08:45:36 +0100

    1er commit page connexion

--- todofilm/src/pages/connexion/connexion.js
+++ todofilm/src/pages/connexion/connexion.js
@@ -1,1 +1,42 @@
- 
+import React, {Component} from 'react';
+import InputComponent from '../../components/InputComponent';
+import Logo from '../../components/Logo';
+import '../../styles/general.css';
+import {datasUser} from '../../datas/datasUser'
+import {datasInputComponent} from '../../datas/datasInputComponent';
+
+import {informationText} from '../../datas/informationText'
+import { Link } from 'react-router-dom';
+
+class connexion extends Component {
+    render() {
+
+    
+        
+        return(
+            <div className='bloc-identify background-yellow'>
+
+                <div className='full-logo'>
+                    <Logo />
+                 </div>
+
+                  <div className='bloc-identify-input'>
+                     <h1>Inscription</h1>
+
+                       <datasInputComponent id='email-input' placeholder='Adresse mail' type='email' recupData={this.recupData} />
+
+                       <datasInputComponent id='passwordInit-input' placeholder='Mot de passe' type='password' recupData={this.recupData} />
+
+                     <Link to="/mot-de-passe-oublie" className="bloc-identify-Mdpo">
+                       {informationText.MdP_Oublie}
+                     </Link>
+
+                     <Link to='/connexion' className='bloc-identify-button'>
+                       <button onClick={this.showData} disabled={verifDisabled}>Valider</button>
+                     </Link>
+                </div>
+            </div>    
+        )
+    }
+}
+export default connexion;
