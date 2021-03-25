39aa32e5a85a3bd8cd52fa941a926f2581db0f35
mdutry <matthieu.dutry@hotmail.fr>
2021-03-25 11:28:03 +0100

    identite.js et css à jour

--- todofilm/src/components/Identite.js
+++ todofilm/src/components/Identite.js
@@ -5,14 +5,6 @@ class Identite extends Component {
 
     render () {
 
-<<<<<<< HEAD
-        const {label, data} = this.props
-
-        return (
-            <div className="bloc-identite">
-                <span className='identite-span'>{label}</span>
-                <p className='identite-par'>{data}</p>
-=======
         const {label, data, id} = this.props
         const classIdentite = id === 'notes' ? 'bloc-identite-notes' : 'bloc-identite'
         const arrayOrNot = Array.isArray(data) ? data.join(', ') : data
@@ -21,7 +13,6 @@ class Identite extends Component {
             <div className={classIdentite}>
                 <span className='identite-span'>{label}</span>
                 <p className='identite-par'>{arrayOrNot}</p>
->>>>>>> 53417f2c1df72891dddc073bd462eb1b08a13e6e
             </div>
         )
     }
