2780df61a2aadd792753c26d1cb036489253179c
Fvandecasteele <fabrice.vandecasteele@sfr.fr>
2021-04-19 15:35:32 +0200

    fix conflict

--- todofilm/src/pages/MdP oublie/MdPOublie.js
+++ todofilm/src/pages/MdP oublie/MdPOublie.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import InputComponent from '../../components/InputComponent';
@@ -30,3 +31,48 @@ class MdPOublie extends Component {
     }
 }
 export default MdPOublie;
+=======
+import React, {Component, context, useContext} from 'react';
+import InputComponent from '../../components/InputComponent';
+import Logo from '../../components/Logo';
+import '../../styles/general.css';
+import {DatasUserContext} from '../../context/DatasUserContext';
+import {DatasInputContext} from '../../context/DatasInputContext';
+import directionMdPOublie from './directionMdPOublie';
+import { Link } from 'react-router-dom';
+import {informationText} from '../../datas/informationText';
+  
+function MdPOublie () {
+​
+  
+    const datasInputContext = useContext(DatasInputContext)
+            
+           
+    return(
+​
+        <div className='bloc-identify background-yellow'>
+​
+                  <div className='full-logo'>
+                               <Logo />
+                       </div>
+​
+               <div className='bloc-identify-input'>
+                               <h1>Mot de passe oublié</h1>
+                     
+                                   <DatasInputContext id='email-input' placeholder='Adresse mail' type='email'  />
+                                   <InputComponent id='email-input' placeholder='Adresse mail' type='email'  />
+​
+                                      {(DatasInputContext.email=== DatasUserContext.email)?
+​
+                         <Link to='/' className='bloc-identify-button button' id='button-MdPOublie' >
+                               <button >Valider</button>
+                         </Link> :
+                                  <button disabled>valider</button> 
+                             }
+               </div>
+        </div>
+      )
+    }
+  
+  export default MdPOublie;
+>>>>>>> Stashed changes
---
+++ todofilm/src/pages/MdP oublie/directionMdPOublie.js
@@ -0,0 +1,25 @@
+import React, {useContext } from 'react';
+import {DatasInputContext} from '../../context/DatasInputContext';
+import { Link } from 'react-router-dom';
+import MdPOublie from './MdPOublie';
+​
+ function directionMdPOublie () {
+    
+   const DatasInputContext = useContext(DatasInputContext)  
+​
+   
+    return (
+        <div>
+                            {(DatasInputContext.email=== DatasUserContext.email )&& usercontext.password('') } 
+​
+                        
+​
+                           <Link to='/connexion'>
+                              <button >Inscription</button>
+                           </Link>   
+                 </div>
+        )
+        
+    }
+​
+export default directionMdPOublie;
