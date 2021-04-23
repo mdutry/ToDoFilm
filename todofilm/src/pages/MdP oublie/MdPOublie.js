14541b825ed06aea234c460351bba202eb622b14
Fvandecasteele <fabrice.vandecasteele@sfr.fr>
2021-04-23 08:56:17 +0200

    maj mdpoublie

--- todofilm/src/pages/MdP oublie/MdPOublie.js
+++ todofilm/src/pages/MdP oublie/MdPOublie.js
@@ -7,33 +7,35 @@ import {DatasInputContext} from '../../context/DatasInputContext';
 import DirectionMdPOublie from './DirectionMdPOublie';
 import { Link } from 'react-router-dom';
 import {informationText} from '../../datas/informationText';
-  
-function MdPOublie () {
 
-    const datasInputContext = useContext(DatasInputContext)
-    const datasUserContext = useContext(DatasUserContext)       
-           
-    return(
-    <div className='bloc-identify background-yellow'>
+function MdPOublie () {
+  
+         const datasInputContext = useContext(DatasInputContext)
+         const datasUserContext = useContext(DatasUserContext) 
+  
+  return(
+            <div className='bloc-identify background-yellow'>
+    
+                    <div className='full-logo'>
+                                <Logo />
+                    </div>
 ​
-                  <div className='full-logo'>
-                               <Logo />
-                       </div>
-​
-               <div className='bloc-identify-input'>
+                <div className='bloc-identify-input'>
+
                                <h1>Mot de passe oublié</h1>
-                     
-                                  
-                                   <InputComponent id='email-input' placeholder='Adresse mail' type='email'  />
+                               
+                               <InputComponent id='email-input' placeholder='Adresse mail' type='email' />
 ​
                                       {(datasInputContext.email=== datasUserContext.email)?
                          <Link to='/connexion' className='bloc-identify-button button' id='button-MdPOublie' >
                                <button >Valider</button>
                          </Link> :
-                                  <button disabled>valider</button> 
+                               <button disabled>valider</button> 
                              }
-               </div>
-        </div>
+
+                             <DirectionMdPOublie />
+                </div>
+            </div>
       )
     }
   
--- todofilm/src/pages/MdP oublie/directionMdPOublie.js
+++ todofilm/src/pages/MdP oublie/directionMdPOublie.js
@@ -1,25 +1,24 @@
 import React, {useContext } from 'react';
 import {DatasInputContext} from '../../context/DatasInputContext';
+import {DatasUserContext} from '../../context/DatasUserContext';
 import { Link } from 'react-router-dom';
 import MdPOublie from './MdPOublie';
 
 function DirectionMdPOublie () {
     
-   const DatasInputContext = useContext(DatasInputContext)
-   const DatasUserContext = useContext(DatasUserContext)
+   const datasInputContext = useContext(DatasInputContext)
+   const datasUserContext = useContext(DatasUserContext)
 
    return (
-        <div>
-                            {(DatasInputContext.email=== DatasUserContext.email )&& usercontext.password('') } 
-​
-                        
-​
-                           <Link to='/connexion'>
-                              <button >Inscription</button>
+                   <div>
+                            {(datasInputContext.email=== datasUserContext.email )  &&  alert (datasInputContext.password) } 
+                            <Link to='/connexion'>
+                              <button >Connexion</button>
                            </Link>   
-                 </div>
+                  </div>
         )
-        
-    }
+                            
+            setTimeout(DirectionMdPOublie, 5000);
+   }
     
     export default DirectionMdPOublie;
